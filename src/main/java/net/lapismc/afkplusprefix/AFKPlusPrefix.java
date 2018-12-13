617ab14e9c17d313499f88d4b4249d48d2e108ad
dart2112 <DNAblue2112@gmail.com>
2018-12-14 00:00:47 +1030

    Initial commit

---
+++ src/main/java/net/lapismc/afkplusprefix/AFKPlusPrefix.java
@@ -0,0 +1,62 @@
+package net.lapismc.afkplusprefix;
+
+import net.lapismc.afkplus.api.AFKStartEvent;
+import net.lapismc.afkplus.api.AFKStopEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+import java.util.UUID;
+
+public final class AFKPlusPrefix extends JavaPlugin implements Listener {
+
+    private Scoreboard board;
+    private Team afkTeam;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        board = Bukkit.getScoreboardManager().getNewScoreboard();
+        afkTeam = board.registerNewTeam("AFK");
+        afkTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+        afkTeam.setPrefix(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix")));
+    }
+
+    private void enableAFK(UUID uuid) {
+        Player p = Bukkit.getPlayer(uuid);
+        afkTeam.addEntry(p.getName());
+    }
+
+    private void disableAFK(UUID uuid) {
+        Player p = Bukkit.getPlayer(uuid);
+        afkTeam.removeEntry(p.getName());
+    }
+
+    @EventHandler
+    public void onAFKStart(AFKStartEvent e) {
+        enableAFK(e.getPlayer().getUUID());
+    }
+
+    @EventHandler
+    public void onAFKStop(AFKStopEvent e) {
+        disableAFK(e.getPlayer().getUUID());
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        e.getPlayer().setScoreboard(board);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        disableAFK(e.getPlayer().getUniqueId());
+    }
+}
